db2c409a241754ad229bcd554d11a3ef09e991d7
SergentK <116554131+SergentK@users.noreply.github.com>
2023-12-15 11:36:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: 'ICOR0UDIB3SCRN2LXYWF', // TODO
+    secretAccessKey: 'fCPfUs1JZEqediS7EvE5emaXlmSc3BCwGEPm5b54', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-73470', // TODO
   },
   suffix: {
     small: '_small.jpg',
